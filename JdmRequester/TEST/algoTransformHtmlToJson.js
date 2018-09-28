2ea07facea5ac26d90b2a2a05c06d0d7d3850e32
mentos126 <henrique-126@gmail.com>
2018-09-28 21:42:31 +0200

    get node and type ordered by weight

--- JdmRequester/TEST/algoTransformHtmlToJson.js
+++ JdmRequester/TEST/algoTransformHtmlToJson.js
@@ -1,34 +1,44 @@
-let text = "";
+function compareEntriesWeight(a, b) {
+    if (parseInt(a[3]) < parseInt(b[3]))
+        return 1;
+    if (parseInt(a[3]) > parseInt(b[3]))
+        return -1;
+    return 0;
+}
+
 
 function loadFileAsText() {
+    let text = "";
     let myFile = document.getElementById("myFile").files[0]
     let fileReader = new FileReader()
     fileReader.onload = fileLoadedEvent => {
         let textFromFileLoaded = fileLoadedEvent.target.result
         text = textFromFileLoaded
-        
+
         let code = text.substring(text.indexOf("<CODE>") + 6, text.indexOf("</CODE>"))
-        
+
         let def = code.substring(code.indexOf("<def>") + 5, code.indexOf("</def>"))
         document.getElementById("def").innerHTML = def
-        
+
         let start = "// les types de noeuds (Nodes Types) : nt;ntid;'ntname'"
         let end = "// les noeuds/termes (Entries) : e;eid;'name';type;w;'formated name' "
         let nodeTypes = code.substring(code.indexOf(start) + start.length, code.indexOf(end))
         nodeTypes = nodeTypes.split("nt;")
         nodeTypes.shift()
-        for(let i in nodeTypes){
-            nodeTypes[i] = nodeTypes[i].slice(0,-1)
-            if(i == nodeTypes.length-1){
-                nodeTypes[i] = nodeTypes[i].slice(0,-2)
+        let nodeTypesRes = []
+        for (let i in nodeTypes) {
+            nodeTypes[i] = nodeTypes[i].slice(0, -1)
+            if (i == nodeTypes.length - 1) {
+                nodeTypes[i] = nodeTypes[i].slice(0, -2)
             }
+            nodeTypesRes.push(nodeTypes[i].split(";"))
         }
 
         let s = "<ul>"
-        for(let i of nodeTypes){
-            s += "<li>"+i+"</li>"
+        for (let i of nodeTypes) {
+            s += "<li>" + i + "</li>"
         }
-        s += "</ul>" 
+        s += "</ul>"
         document.getElementById("node_type").innerHTML = s
 
         start = end
@@ -36,20 +46,37 @@ function loadFileAsText() {
         let entries = code.substring(code.indexOf(start) + start.length, code.indexOf(end))
         entries = entries.split("e;")
         entries.shift()
-        for(let i in entries){
-            entries[i] = entries[i].slice(0,-1)
-            if(i == entries.length-1){
-                entries[i] = entries[i].slice(0,-2)
+        let entriesRes = []
+        for (let i in entries) {
+            entries[i] = entries[i].slice(0, -1)
+            if (i == entries.length - 1) {
+                entries[i] = entries[i].slice(0, -2)
             }
+            entriesRes.push(entries[i].split(";"))
         }
         s = "<ul>"
-        for(let i of entries){
-            s += "<li>"+i+"</li>"
+        for (let i of entries) {
+            s += "<li>" + i + "</li>"
         }
-        s += "</ul>" 
+        s += "</ul>"
         document.getElementById("entries").innerHTML = s
 
-        console.log(nodeTypes)
+        let entriesNodeTypesRes = []
+        for (let entRes of entriesRes) {
+            for (let r of nodeTypesRes) {
+                if (r[0] == entRes[2]) {
+                    let temp = { ...entRes
+                    }
+                    temp[2] = r[1]
+                    entriesNodeTypesRes.push(temp)
+                    break
+                }
+            }
+        }
+
+        entriesNodeTypesRes.sort(compareEntriesWeight)
+        console.log(entriesRes)
+        console.log(entriesNodeTypesRes)
 
         /*start = end
         end = "// les relations sortantes : r;rid;node1;node2;type;w "
@@ -87,10 +114,8 @@ function loadFileAsText() {
         s += "</ul>" 
         document.getElementById("rel_in").innerHTML = s*/
 
-        
+
     }
     fileReader.readAsText(myFile, "UTF-8")
 
 }
-
-
