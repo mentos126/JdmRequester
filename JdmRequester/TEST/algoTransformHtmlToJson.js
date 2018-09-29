6d39103ab91954e22c7cda12a342fad3f9a13638
mentos126 <henrique-126@gmail.com>
2018-09-29 13:07:30 +0200

    mise en relation les noeuds et leur relations

--- JdmRequester/TEST/algoTransformHtmlToJson.js
+++ JdmRequester/TEST/algoTransformHtmlToJson.js
@@ -1,3 +1,16 @@
+//label utile pour parser le code
+const labelCodeInit = "<CODE>"
+const labelCodeEnd = "</CODE>"
+const labelDefInit = "<def>"
+const labelDefEnd = "</def>"
+const labelTypesNode = "// les types de noeuds (Nodes Types) : nt;ntid;'ntname'"
+const labelEntries = "// les noeuds/termes (Entries) : e;eid;'name';type;w;'formated name' "
+const labelRelationsType = "// les types de relations (Relation Types) : rt;rtid;'trname';'trgpname';'rthelp' "
+const labelRelationOut = "// les relations sortantes : r;rid;node1;node2;type;w "
+const labelRelationIn = "// les relations entrantes : r;rid;node1;node2;type;w "
+const labelEnd = "// END"
+
+//fonction de comparaison des nodes pour sort
 function compareEntriesWeight(a, b) {
     if (parseInt(a[3]) < parseInt(b[3]))
         return 1;
@@ -6,22 +19,40 @@ function compareEntriesWeight(a, b) {
     return 0;
 }
 
+//fonction de comparaison des relations pour sort
+function compareRelationsWeight(a, b) {
+    if (parseInt(a.w) < parseInt(b.w))
+        return 1;
+    if (parseInt(a.w) > parseInt(b.w))
+        return -1;
+    return 0;
+}
 
+
+//li le texte dans l'input
 function loadFileAsText() {
     let text = "";
     let myFile = document.getElementById("myFile").files[0]
     let fileReader = new FileReader()
     fileReader.onload = fileLoadedEvent => {
+        //recupere text du fichier
         let textFromFileLoaded = fileLoadedEvent.target.result
         text = textFromFileLoaded
 
-        let code = text.substring(text.indexOf("<CODE>") + 6, text.indexOf("</CODE>"))
+        //TODO hash here in MD5
+        let hashedText = "MD5"
 
-        let def = code.substring(code.indexOf("<def>") + 5, code.indexOf("</def>"))
-        document.getElementById("def").innerHTML = def
+        //recupere code utilisable du text
+        let code = text.substring(text.indexOf(labelCodeInit) + labelCodeInit.length, text.indexOf(labelCodeEnd))
+        let copyCode = code.slice(0, code.length - 1)
 
-        let start = "// les types de noeuds (Nodes Types) : nt;ntid;'ntname'"
-        let end = "// les noeuds/termes (Entries) : e;eid;'name';type;w;'formated name' "
+        //recupere definition du code
+        let def = code.substring(code.indexOf(labelDefEnd) + labelDefInit.length, code.indexOf(labelDefEnd))
+        //document.getElementById("def").innerHTML = def
+
+        //decoupe le code pour en sortir une liste de type de noeud
+        let start = labelTypesNode
+        let end = labelEntries
         let nodeTypes = code.substring(code.indexOf(start) + start.length, code.indexOf(end))
         nodeTypes = nodeTypes.split("nt;")
         nodeTypes.shift()
@@ -34,15 +65,9 @@ function loadFileAsText() {
             nodeTypesRes.push(nodeTypes[i].split(";"))
         }
 
-        let s = "<ul>"
-        for (let i of nodeTypes) {
-            s += "<li>" + i + "</li>"
-        }
-        s += "</ul>"
-        //document.getElementById("node_type").innerHTML = s
-
+        //decoupe le code pour en sortir une liste de noeuds
         start = end
-        end = "// les types de relations (Relation Types) : rt;rtid;'trname';'trgpname';'rthelp' "
+        end = labelRelationsType
         let entries = code.substring(code.indexOf(start) + start.length, code.indexOf(end))
         entries = entries.split("e;")
         entries.shift()
@@ -54,14 +79,9 @@ function loadFileAsText() {
             }
             entriesRes.push(entries[i].split(";"))
         }
-        s = "<ul>"
-        for (let i of entries) {
-            s += "<li>" + i + "</li>"
-        }
-        s += "</ul>"
-        //document.getElementById("entries").innerHTML = s
 
-        /*let entriesNodeTypesRes = []
+        //transforme la liste de noeud pour y mettre le type de noeud
+        let entriesNodeTypesRes = []
         for (let entRes of entriesRes) {
             for (let r of nodeTypesRes) {
                 if (r[0] == entRes[2]) {
@@ -73,46 +93,155 @@ function loadFileAsText() {
                 }
             }
         }
-
         entriesNodeTypesRes.sort(compareEntriesWeight)
-        console.log(entriesNodeTypesRes)*/
 
+        //decoupe le code pour en recouperer le type de relations
+        code = copyCode.slice(0, copyCode.length - 1)
         start = end
-        end = "// les relations sortantes : r;rid;node1;node2;type;w "
+        end = labelRelationOut
         let relationTypes = code.substring(code.indexOf(start) + start.length, code.indexOf(end))
         relationTypes = relationTypes.split("rt;")
         relationTypes.shift()
-        s = "<ul>"
-        for(let i of relationTypes){
-            s += "<li>"+i+"</li>"
+        let relationTypesRes = []
+        for (let i in relationTypes) {
+            relationTypes[i] = relationTypes[i].slice(0, -1)
+            if (i == relationTypes.length - 1) {
+                relationTypes[i] = relationTypes[i].slice(0, -2)
+            }
+            relationTypesRes.push(relationTypes[i].split(";"))
         }
-        s += "</ul>" 
-        document.getElementById("relation_types").innerHTML = s
 
-        /*start = end
-        end = "// les relations entrantes : r;rid;node1;node2;type;w "
+        //decoupe le code pour en sortir une liste de relation sortante
+        start = end
+        end = labelRelationIn
         let relOut = code.substring(code.indexOf(start) + start.length, code.indexOf(end))
         relOut = relOut.split("r;")
         relOut.shift()
-        s = "<ul>"
-        for(let i of relOut){
-            s += "<li>"+i+"</li>"
+        let relOutRes = []
+        for (let i in relOut) {
+            relOut[i] = relOut[i].slice(0, -1)
+            if (i == relOut.length - 1) {
+                relOut[i] = relOut[i].slice(0, -2)
+            }
+            relOutRes.push(relOut[i].split(";"))
         }
-        s += "</ul>" 
-        document.getElementById("rel_out").innerHTML = s
 
+        //trouve le noeud demandé
+        let theNode = {}
+        for (let n of entriesNodeTypesRes) {
+            if (parseInt(n[0]) == parseInt(relOutRes[0][1])) {
+                theNode = {
+                    eid: n[0],
+                    name: n[1],
+                    ntname: n[2],
+                    w: n[3],
+                    formatedName: n[4]
+                }
+                break
+            }
+        }
+
+        //transforme la liste de relation sortante pour y mettre le type de relation
+        let relOutTypesRes = []
+        for (let ro of relOutRes) {
+            let temp = {
+                rid: ro[0],
+                node1: ro[1],
+                node2: ro[2],
+                type: ro[3],
+                w: ro[4]
+            }
+            temp.node1 = { ...theNode
+            }
+            for (let n of entriesNodeTypesRes) {
+                if (parseInt(n[0]) == parseInt(temp.node2)) {
+                    temp.node2 = {
+                        eid: n[0],
+                        name: n[1],
+                        ntname: n[2],
+                        w: n[3],
+                        formatedName: n[4]
+                    }
+                    break
+                }
+            }
+            for (let rt of relationTypesRes) {
+                if (rt[0] == ro[3]) {
+                    temp.type = {
+                        trname: rt[1],
+                        trgpname: rt[2],
+                        rthelp: rt[3]
+                    }
+                    break
+                }
+            }
+            relOutTypesRes.push(temp)
+        }
+        relOutTypesRes.sort(compareRelationsWeight)
+
+        //decoupe le code pour en sortir une liste de relation entrante
         start = end
-        end = "// END"
+        end = labelEnd
         let relIn = code.substring(code.indexOf(start) + start.length, code.indexOf(end))
         relIn = relIn.split("r;")
         relIn.shift()
-        s = "<ul>"
-        for(let i of relIn){
-            s += "<li>"+i+"</li>"
+        let relInRes = []
+        for (let i in relIn) {
+            relIn[i] = relIn[i].slice(0, -1)
+            if (i == relIn.length - 1) {
+                relIn[i] = relIn[i].slice(0, -2)
+            }
+            relInRes.push(relIn[i].split(";"))
         }
-        s += "</ul>" 
-        document.getElementById("rel_in").innerHTML = s*/
 
+        //transforme la liste de relation sortante pour y mettre le type de relation
+        let relInTypesRes = []
+        for (let ro of relInRes) {
+            let temp = {
+                rid: ro[0],
+                node1: ro[1],
+                node2: ro[2],
+                type: ro[3],
+                w: ro[4]
+            }
+            for (let n of entriesNodeTypesRes) {
+                if (parseInt(n[0]) == parseInt(temp.node1)) {
+                    temp.node1 = {
+                        eid: n[0],
+                        name: n[1],
+                        ntname: n[2],
+                        w: n[3],
+                        formatedName: n[4]
+                    }
+                    break
+                }
+            }
+            temp.node2 = { ...theNode
+            }
+            for (let rt of relationTypesRes) {
+                if (rt[0] == ro[3]) {
+                    temp.type = {
+                        trname: rt[1],
+                        trgpname: rt[2],
+                        rthelp: rt[3]
+                    }
+                    break
+                }
+            }
+            relInTypesRes.push(temp)
+        }
+        relInTypesRes.sort(compareRelationsWeight)
+
+
+        let RESULTAT = {
+            node: theNode,
+            hashMD5: hashedText,
+            relIn:relInTypesRes,
+            relOut: relOutTypesRes
+        }
+        console.log(RESULTAT)
+
+        return RESULTAT;
 
     }
     fileReader.readAsText(myFile, "UTF-8")
